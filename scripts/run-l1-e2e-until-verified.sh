3b27412abe319d297b460fa933d66be4adfd59f2
ekulkisnek <lkensik8@gmail.com>
2026-05-29 16:04:38 -0500

    wip: fix L1 E2E simulator blockers (post_fund_relaunch=1 + skip android seed with known addr tb1qewdkqej3xc6hh2v5q88rnaekd2zkccf0zq6kdf for until-verified)

--- scripts/run-l1-e2e-until-verified.sh
+++ scripts/run-l1-e2e-until-verified.sh
@@ -103,7 +103,9 @@ else
   set +e
   L1_E2E_SKIP_LOCK=1 \
     L1_IOS_SIM_ANDROID_E2E_LOG_DIR="$IOS_SIM_RUN" \
-    L1_E2E_POST_FUND_RELAUNCH=0 \
+    L1_E2E_POST_FUND_RELAUNCH=1 \
+    REDWALLET_SKIP_ANDROID_SEED=1 \
+    ANDROID_L1_RECEIVE_ADDRESS=tb1qewdkqej3xc6hh2v5q88rnaekd2zkccf0zq6kdf \
     bash "$ROOT_DIR/scripts/run-l1-ios-simulator-to-android-phone-e2e.sh" "$@"
   ios_to_android_rc=$?
   set -e
@@ -117,7 +119,9 @@ else
   set +e
   L1_E2E_SKIP_LOCK=1 \
     L1_ANDROID_IOS_SIM_E2E_LOG_DIR="$ANDROID_SIM_RUN" \
-    L1_E2E_POST_FUND_RELAUNCH=0 \
+    L1_E2E_POST_FUND_RELAUNCH=1 \
+    REDWALLET_SKIP_ANDROID_SEED=1 \
+    ANDROID_L1_RECEIVE_ADDRESS=tb1qewdkqej3xc6hh2v5q88rnaekd2zkccf0zq6kdf \
     bash "$ROOT_DIR/scripts/with-android-build-env.sh" "$ROOT_DIR/scripts/run-l1-android-phone-to-ios-simulator-e2e.sh" "$@"
   android_to_ios_rc=$?
   set -e
